0969762460ec47ab1e6edbf49111ff715583a28b
arspa <arspa@192.168.0.13>
2022-10-30 18:24:45 -0400

    first commit

---
+++ src/main/java/com/spacex/GitDemoProjectV1/GitDemoProjectV1Application.java
@@ -0,0 +1,13 @@
+package com.spacex.GitDemoProjectV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProjectV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProjectV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spacex/GitDemoProjectV1/GitDemoProjectV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacex.GitDemoProjectV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProjectV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
